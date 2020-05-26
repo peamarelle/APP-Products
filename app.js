f82c8b7cc8d791a0cd3de3c409db9c0c1ea12595
peamarelle <peamarelle@gmail.com>
2020-05-26 17:30:08 -0300

    se agregan elementos en el DOM y se eliminan elementos

--- app.js
+++ app.js
@@ -8,29 +8,48 @@ class Product {
 
 class UI {
 
-    addProduct() {
-        document.getElementById('product-list');
+    static addProduct(product) {
+        const productList = document.getElementById('product-list');
         const element = document.createElement('div');
-
+        element.innerHTML = `
+            <div class="card-body">
+                <strong>Product Name: </strong>${product.name}
+                <strong>Product Price: </strong>${product.price}
+                <strong>Product Year: </strong>${product.year}
+                <a class="btn btn-danger" name="delete" href="#">Delete</a>
+            </div>
+        `;
+        productList.appendChild(element);
     }
 
-    deleteProduct() {
-
+    static resetForm() {
+        document.getElementById('product-form').reset();
     }
 
-    showMessage() {
+    static deleteProduct(element) {
+        if(element.name==="delete") {
+            element.parentElement.parentElement.remove();
+        }
+    }
+
+    static showMessage() {
 
     }
 
 }
 //DOM EVENTS
 
-document.getElementById('products-form').addEventListener('submit', (e) => {
+document.getElementById('product-form').addEventListener('submit', e => {
     const name = document.getElementById('name').value;
     const price = document.getElementById('price').value;
     const year = document.getElementById('year').value;
 
     const product = new Product(name, price, year);
+    UI.addProduct(product);
+    UI.resetForm();//limpio los datos del formulario para poder ingresar nuevos datos
+    e.preventDefault();//este evento cancela el autorefresh del formulario. El auto refresh se utiliza para luego recibir la respuesta del servidor
+});
 
-    e.preventDefault();
+document.getElementById('product-list').addEventListener('click', e => {
+    UI.deleteProduct(e.target);
 });
